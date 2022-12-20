96ff5155cfad67166a9793a106bd629459d31975
valya <vanilik@mail.ru>
2022-12-20 23:41:13 +0300

    Задача 2

---
+++ Examples/Examples002/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(numberA > numberB)
+{
+    Console.Write("Максимум = ");
+    Console.WriteLine(max=numberA);
+}
+else
+{
+    Console.Write("Максимум = ");
+    Console.WriteLine(max=numberB);
+}
