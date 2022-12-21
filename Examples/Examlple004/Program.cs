06f9b2b6c7d4eff0ab80d712f4b96af632dd197e
valya <vanilik@mail.ru>
2022-12-21 22:21:39 +0300

    Задача 4, найдена и исправлена ошибка

--- Examples/Examlple004/Program.cs
+++ Examples/Examlple004/Program.cs
@@ -1,19 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
 int numberC = Convert.ToInt32(Console.ReadLine());
 
-int max = numberA;
+int max = 0;
 
-if(max < numberB)
-{ 
-    max = numberB;
-}
-if(max < numberC);
-{
-    max = numberC;
-}
+if(max < numberA) max = numberA;
+
+if(max < numberB) max = numberB;
+if(max < numberC) max = numberC;
 
 Console.Write("Макисмальное число = ");
 Console.WriteLine(max);
