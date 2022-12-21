e0f16716ac0b284b324001a16e94c8ebcd483c62
valya <vanilik@mail.ru>
2022-12-21 23:06:42 +0300

    Не помню сохранила или нет

--- Examples/Examples002/Program.cs
+++ Examples/Examples002/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 int max = 0;
 int min = 0;
